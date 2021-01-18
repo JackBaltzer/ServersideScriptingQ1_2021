6539852f4c70bb97eef56be2bab998c4afd5eced
Jack Baltzer <jabb@tec.dk>
2021-01-18 11:09:27 +0100

    tilføjet EJS

--- app.js
+++ app.js
@@ -2,8 +2,30 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
  
+const logger = require("morgan");
+app.use(logger("dev", {
+   // hvis ALLE requests skal ses i loggen, kan næste linje udkommenteres
+   skip: req => !req.url.endsWith(".html") && req.url.indexOf(".") > -1
+}));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World, det er koldt i dag');
+
+	res.render('index', { 
+		titel : "Hello World, <em>det er koldt i dag</em>",
+		indhold: "indholdet",
+		etTal: 123,
+		
+	});
+
 }); 
 
-app.listen(port);
+app.use(express.static('public'));
+
+app.listen(port, function(err){
+	if(err) console.log(err);
+	console.log('Serveren kører! er tilgængelig: http://localhost:' + port);
+});
